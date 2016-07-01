5a9b997e909d749c479012d43bdf62d649b3f34b
hpkmw <clwu001@gmail.com>
2016-07-01 17:19:58 +0800

    step1

--- modules/pagelayout.js
+++ modules/pagelayout.js
@@ -14,6 +14,21 @@ export class Step1 extends React.Component {
             <div className="col-md-12">
               <h4>{this.props.t1}</h4>
               <div>Step 01 -- {this.props.t2}</div>
+              <div>
+                請輸入事件標題<br/>
+                <input type="text" placeholder="事件的標題"></input><br/>
+                請選擇查看訊息後需執行之動作<br/>
+                <select>
+                  <option value="純文字">純文字</option>
+                </select><br/>
+                請輸入執行動作內容<br/>
+                <textarea rows="8" placeholder="執行動作內容"></textarea><br/>
+                請選擇Application ID<br/>
+                <input type="text" placeholder="iOS App ID"></input><br/>
+                <input type="text" placeholder="Android App ID"></input><br/>
+                請選擇觸發事件之人員<br/>
+                <input type="text" placeholder="人員"></input>
+              </div>
             </div>
           </div>
           {/* Content from caller's children: to specialize this page */}
